7d73b2bb62d5dff328af36502a0d1e84a5080347
LiloueG <liloue.guyot25@gmail.com>
2024-12-18 12:45:36 +0100

    feat meta desc and title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 import { apiEndpoint, repositoryName } from "./slicemachine.config.json";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/prismic"],
 
-  css: ['/public/css/main.css'],
+  css: ["/public/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -14,6 +14,19 @@ export default defineNuxtConfig({
   },
 
   prismic: {
-    endpoint: apiEndpoint || repositoryName
-  }
-})
+    endpoint: apiEndpoint || repositoryName,
+  },
+
+  app: {
+    head: {
+      title: "Portfolio de Liloue Guyot - Webdesign et Création Numérique",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Bienvenue sur mon portfolio ! Je suis Liloue Guyot, designer graphique spécialisée en webdesign et création numérique. Découvrez mes projets, ma passion pour l'illustration et mes compétences en design interactif.",
+        },
+      ],
+    },
+  },
+});
